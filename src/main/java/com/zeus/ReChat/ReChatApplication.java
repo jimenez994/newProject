c48fda1a365d044ca1fb8b493f208f8caccbcf3d
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-06-10 16:03:14 -0400

    first commit

---
+++ src/main/java/com/zeus/ReChat/ReChatApplication.java
@@ -0,0 +1,12 @@
+package com.zeus.ReChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReChatApplication.class, args);
+	}
+}
